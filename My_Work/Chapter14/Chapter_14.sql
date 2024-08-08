92acc9750554c088e30d5557250eb1129b92c4ec
Pierre <jonkerpierre637@gmail.com>
2024-08-08 12:51:28 +0200

    Update Chapter_14.sql

--- My_Work/Chapter14/Chapter_14.sql
+++ My_Work/Chapter14/Chapter_14.sql
@@ -34,7 +34,7 @@ SELECT ST_LineFromText('LINESTRING(-105.90 35.67,-105.91 35.67)', 4326);
 SELECT ST_MakeLine(ST_MakePoint(-74.92, 42.69), ST_MakePoint(-74.12, 42.45));
 
 -- Listing 14-8: Functions specific to making Polygons
-SELECT ST_PolygonFromText('POLYGON((-74.9 42.7, -75.1 42.7, -75.1 42.6, -74.9 42.7))', 4326);
+6;
 SELECT ST_MakePolygon(ST_GeomFromText('LINESTRING(-74.92 42.7, -75.06 42.71, -75.07 42.64, -74.92 42.7)', 4326));
 SELECT ST_MPolyFromText('MULTIPOLYGON(( (-74.92 42.7, -75.06 42.71, -75.07 42.64, -74.92 42.7), (-75.0 42.66, -75.0 42.64, -74.98 42.64, -74.98 42.66, -75.0 42.66)))', 4326);
 
